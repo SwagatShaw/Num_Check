92a1ce4f7ad09f5fba76f6f98e1768433c60de13
Swagat Shaw <montyshaw26@yahoo.com>
2018-01-08 12:46:13 -0500

    added palindrome

---
+++ palindrome.cpp
@@ -0,0 +1,25 @@
+int palindrome()
+{
+     int n, a, digit, rev = 0;
+
+     cout << "Enter a positive number: ";
+     cin >> a;
+
+     n = a;
+
+     do
+     {
+         digit = a % 10;
+         rev = (rev * 10) + digit;
+         a = a / 10;
+     } while (a != 0);
+
+     cout << " The reverse of the number is: " << rev << endl;
+
+     if (n == rev)
+         cout << " The number is a palindrome";
+     else
+         cout << " The number is not a palindrome";
+
+    return 0;
+}
